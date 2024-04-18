683f7105fa8d1d7c95627b6b5c73174729dc4b8f
victor paul arony <98453453+VictorPaulArony@users.noreply.github.com>
2024-04-18 15:09:48 +0300

    Update accum.go

--- accum.go
+++ accum.go
@@ -28,3 +28,11 @@ func Accum(words string) string {
 		fmt.Printf("accum(\"%s\") -> \"%s\"\n", testCase, Accum(testCase))
 	}
 }
+
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
