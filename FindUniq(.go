c34ba26647f7e0f6b0b1d4952dbb62025784805d
victor paul arony <98453453+VictorPaulArony@users.noreply.github.com>
2024-04-19 15:15:07 +0300

    Update FindUniq(.go

--- FindUniq(.go
+++ FindUniq(.go
@@ -20,6 +20,18 @@ func FindUniq(arr []float32) {
     }
 
     // Output the unmatched float32s
+
+
+    
+    //  var uniq float32
+    // for num, count := range counts {
+    //     if count == 1 {
+    //         uniq = num
+    //         break // Found the unique number, so exit the loop
+    //     }
+
+
+    
     for _, ar := range arr {
         if match[ar] == 1 {
             fmt.Println(ar)
