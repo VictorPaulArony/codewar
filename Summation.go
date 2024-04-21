9dd3c059f2c57908a29f93fcead9810275d09b00
victor paul arony <98453453+VictorPaulArony@users.noreply.github.com>
2024-04-21 13:37:57 +0300

    Update Summation.go

--- Summation.go
+++ Summation.go
@@ -23,6 +23,13 @@ func summation(num int) int {
 //     return n * (n + 1) / 2
 // }
 
+// func Summation(n int) (sum int) {
+// 	for i := 0; i <= n; i++ {
+// 		sum += i
+// 	}
+// 	return
+// }
+
 func main() {
 	// Test cases
 	fmt.Println(summation(5)) // Output: 15 (1 + 2 + 3 + 4 + 5 = 15)
