f8cd1517c2eecb86d2ad2f3030e3ad5d9789898c
victor paul arony <98453453+VictorPaulArony@users.noreply.github.com>
2024-04-22 10:02:22 +0300

    Create doubled_array_value.go

---
+++ doubled_array_value.go
@@ -0,0 +1,22 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+package main
+
+import "fmt"
+
+func doubleArray(arr []int) []int {
+    doubled := make([]int, len(arr))
+    for i, num := range arr {
+        doubled[i] = num * 2
+    }
+    return doubled
+}
+
+func main() {
+    arr := []int{1, 2, 3, 4, 5}
+    doubled := doubleArray(arr)
+    fmt.Println(doubled) // Output: [2 4 6 8 10]
+}
