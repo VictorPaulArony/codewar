2d6712564eae5278bddcc1f99a13a522b747bebe
victor paul arony <98453453+VictorPaulArony@users.noreply.github.com>
2024-04-22 17:02:50 +0300

    Update GetGrade.go

--- GetGrade.go
+++ GetGrade.go
@@ -29,6 +29,26 @@ func GetGrade(a, b, c int) rune {
 	}
 }
 
+
+
+// func GetGrade(a, b, c int) rune {
+// 	// Calculate the average of the three scores
+// 	average := (a + b + c) / 3
+
+// 	// Determine the letter grade based on the average score
+// 	if average >= 90 && average <= 100 {
+// 		return 'A'
+// 	} else if average >= 80 && average < 90 {
+// 		return 'B'
+// 	} else if average >= 70 && average < 80 {
+// 		return 'C'
+// 	} else if average >= 60 && average < 70 {
+// 		return 'D'
+// 	} else {
+// 		return 'F'
+// 	}
+// }
+
 func main() {
 	// Test cases
 	grade := GetGrade(85, 90, 88)
