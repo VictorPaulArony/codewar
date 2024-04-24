4168ea05c4de2c23fd649af0ac4b60dfb0ea98e9
victor paul arony <98453453+VictorPaulArony@users.noreply.github.com>
2024-04-24 12:53:09 +0300

    Create count_add_string.go

---
+++ count_add_string.go
@@ -0,0 +1,16 @@
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+package kata
+
+import (
+    "strings"
+    "strconv"
+)
+
+func countSheep(num int) string {
+    res := ""
+    for i := 1; i <= num; i++ {
+        n := strconv.Itoa(i)
+        res += n + " sheep..."
+    }
+    return res
+}
