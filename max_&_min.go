7280332bd5ebceaeb1e5b339453744342f374a72
victor paul arony <98453453+VictorPaulArony@users.noreply.github.com>
2024-04-29 09:52:33 +0300

    Create max_&_min.go

---
+++ max_&_min.go
@@ -0,0 +1,22 @@
+finding the maximun and minimum values in a given array
+package main 
+import "fmt"
+
+func MinMax(arr []int) (int,int) {
+    min := arr[0]
+    max := arr[0]
+    for _, num := range arr {
+        if num < min {
+            min = num 
+        }
+        if num >max {
+            max = num
+        }
+    }
+    return min, max
+}
+func main() {
+    arr := []int{3, 5, 10, 9, 2, 7}
+    min, max := MinMax(arr)
+    fmt.Printf("Minimum: %d, Maximum: %d\n", min, max)
+}
