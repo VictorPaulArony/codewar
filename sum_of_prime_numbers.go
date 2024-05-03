3804a316554f13f1130b792c1701c19816f1aab2
viarony <viaronypaularony@gmail.com>
2024-05-03 15:18:26 +0300

    code

--- sum_of_prime_numbers.go
+++ sum_of_prime_numbers.go
@@ -1,33 +1,29 @@
 package main
 
-import (
-	"fmt"
-)
-
 func isPrime(num int) bool {
- if num <= 1 {
-  return false
- }
- for i := 2; i*i <= num; i++ {
-  if num%i == 0 {
-   return false
-  }
- }
- return true
+	if num <= 1 {
+		return false
+	}
+	for i := 2; i*i <= num; i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
 }
 
 func sumOfPrimes(num int) int {
- sum := 0
- for i := 2; i <= num; i++ {
-  if isPrime(i) {
-   sum += i
-  }
- }
- return sum
+	sum := 0
+	for i := 2; i <= num; i++ {
+		if isPrime(i) {
+			sum += i
+		}
+	}
+	return sum
 }
 
-func main() {
- number := 1000000
- result := sumOfPrimes(number)
- fmt.Printf("Sum of prime numbers up to %d is: %d", number, result)
-}
+// func main() {
+//  number := 1000000
+//  result := sumOfPrimes(number)
+//  fmt.Printf("Sum of prime numbers up to %d is: %d", number, result)
+// }
