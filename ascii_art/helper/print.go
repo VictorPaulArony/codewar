dc0ac29eba7ca159a03695240afeac8833271285
victorpaularony <smallypauls@gmail.com>
2024-05-06 12:08:01 +0300

    test

---
+++ ascii_art/helper/print.go
@@ -0,0 +1,18 @@
+package helper
+
+import (
+	"fmt"
+	"strings"
+)
+
+func printer(str string) []string {
+	var res []string
+	if str == "" {
+		fmt.Println("EMPTY STRING")
+	}
+	for _, char := range str {
+		first := int(char-32)*9 - 2
+		res = append(res, first)
+	}
+	fmt.Print(strings.Join(res, "\n"))
+}
