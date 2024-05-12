8aa44d1362726238064ea2e86fc07d615f918750
victorpaularony <smallypauls@gmail.com>
2024-05-12 18:45:50 +0300

    update

--- volume_and_surfacearea/main.go
+++ volume_and_surfacearea/main.go
@@ -1,2 +1,11 @@
 package main
-func GetSize(w,h,d int) [2]int {
+
+func GetSize(w, h, d int) [2]int {
+	var area int
+	var vol int
+	var res [2]int
+	area = 2*w*h + 2*h*d + 2*w*d
+	vol = w * h * d
+	res = [2]int{area, vol}
+	return res
+}
