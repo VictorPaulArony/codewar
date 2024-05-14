979134b6c5f45491dfd49c96620238db6c0172ae
victorpaularony <smallypauls@gmail.com>
2024-05-14 22:52:12 +0300

    find the odd number

--- find_odd.go
+++ find_odd.go
@@ -12,15 +12,15 @@ Examples
 package main
 
 func FindOdd(seq []int) int {
-  var res int
-  count := make(map[int]int)
-   for _ , val := range seq {
-     count[val] ++
-   }
-  for i, v := range count {
-    if v%2 != 0 {
-      res = i
-    }
-  }
-  return res
+	var res int
+	count := make(map[int]int)
+	for _, val := range seq {
+		count[val]++
+	}
+	for i, v := range count {
+		if v%2 != 0 {
+			res = i
+		}
+	}
+	return res
 }
