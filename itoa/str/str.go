26a33a6124490b3219ab93052e3c9b8f8acf27a1
victorpaularony <smallypauls@gmail.com>
2024-05-14 22:49:48 +0300

    package folder

---
+++ itoa/str/str.go
@@ -0,0 +1,11 @@
+package str
+
+import "fmt"
+
+func IntToString1(num int) string {
+	return fmt.Sprintf("%d", num)
+}
+
+// func intToString2(num int) string {
+//     return strconv.Itoa(num)
+// }
