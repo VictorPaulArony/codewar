205c379750945dbebbb039f62479c74bfb268d98
victorpaularony <smallypauls@gmail.com>
2024-05-15 01:09:10 +0300

    initials

---
+++ intials/init/init.go
@@ -0,0 +1,11 @@
+package main
+
+import "strings"
+
+func Initials(name string) string {
+	// Split the name into first and last name
+	parts := strings.Split(name, " ")
+	// Extract the first letter of each part and convert to uppercase
+	initials := strings.ToUpper(string(parts[0][0])) + "." + strings.ToUpper(string(parts[1][0]))
+	return initials
+}
