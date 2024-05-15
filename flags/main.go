0c822b7f2259d08825106690af49a6b543207c57
victorpaularony <smallypauls@gmail.com>
2024-05-15 15:52:16 +0300

    adding more .txt files to test various output of the flags

--- flags/main.go
+++ flags/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"os"
 	"fmt"
+	"os"
 	"strings"
 )
 
@@ -11,10 +11,42 @@ func main() {
 		panic("INVALID ARGUMENT FORMAT")
 	}
 	if strings.HasSuffix(os.Args[2], ".txt") {
-		err := os.WriteFile(os.Args[2], []byte(os.Args[1] + "\n"), 0o644)
+		err := os.WriteFile(os.Args[2], []byte(os.Args[1]+"\n"), 0o644)
 		if err != nil {
 			panic("INVALID")
 		}
 		fmt.Printf("This %s file has been created \n", os.Args[2])
+	} else {
+		if strings.Contains(string(os.Args[2]), "--upper") {
+			str, err := os.ReadFile(os.Args[1]) 
+			if err != nil {
+				panic("INVALID")
+			}
+			err = os.WriteFile(os.Args[1], []byte(strings.ToUpper(string(str))), 0o644)
+			if err != nil {
+				panic("INVALID")
+			}
+			fmt.Printf("This %s file has been modified \n", os.Args[2])
+		} else if strings.Contains(string(os.Args[2]), "--lower") {
+			str, err := os.ReadFile(os.Args[1]) 
+			if err != nil {
+				panic("INVALID")
+			}
+			err = os.WriteFile(os.Args[1], []byte(strings.ToLower(string(str))), 0o644)
+			if err != nil {
+				panic("INVALID")
+			}
+			fmt.Printf("This %s file has been modified \n", os.Args[2])
+		} else if strings.Contains(string(os.Args[2]), "--cap") {
+			str, err := os.ReadFile(os.Args[1]) 
+			if err != nil {
+				panic("INVALID")
+			}
+			err = os.WriteFile(os.Args[1], []byte(strings.Title(string(str))), 0o644)
+			if err != nil {
+				panic("INVALID")
+			}
+			fmt.Printf("This %s file has been modified \n", os.Args[2])
+		}
 	}
 }
