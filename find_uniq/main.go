fc324f0796950aabd6cc9c4f259eabf613cd79d5
victorpaularony <smallypauls@gmail.com>
2024-05-15 20:53:59 +0300

    main file for the find_uniq directory

---
+++ find_uniq/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	// Example input
+	input := []float32{1.1, 2.2, 3.3, 2.2, 4.4, 5.5, 1.1, 3.3}
+
+	// Find and print the first unmatched float32
+	uniq.FindUniq(input)
+}
