48179d70f77b800acc2dac027d2f1954ae89f9b4
victorpaularony <smallypauls@gmail.com>
2024-05-19 15:41:29 +0300

    deleted GetCount.go file

--- count_vowels/main.go
+++ count_vowels/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"count_vowels/vowels"
 )
 
 func main() {
 	// Test cases
-	fmt.Println(countVowels("hello"))       // Output: 2
-	fmt.Println(countVowels("world"))       // Output: 1
-	fmt.Println(countVowels("programming")) // Output: 3
+	fmt.Println(vowels.CountVowels("hello"))       // Output: 2
+	fmt.Println(vowels.CountVowels("world"))       // Output: 1
+	fmt.Println(vowels.CountVowels("programming")) // Output: 3
 }
