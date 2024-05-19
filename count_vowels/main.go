efa087221c5155228ee4a97f8796524ffd7cb18d
victorpaularony <smallypauls@gmail.com>
2024-05-19 15:16:02 +0300

    main file for the count_vowels program

---
+++ count_vowels/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Test cases
+	fmt.Println(countVowels("hello"))       // Output: 2
+	fmt.Println(countVowels("world"))       // Output: 1
+	fmt.Println(countVowels("programming")) // Output: 3
+}
