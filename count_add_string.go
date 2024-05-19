39218b0795d5af562ab2990f147062c0223a8e0a
victorpaularony <smallypauls@gmail.com>
2024-05-19 15:11:53 +0300

    deleted initials.go file

--- count_add_string.go
+++ count_add_string.go
@@ -1,8 +1,8 @@
 // Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
-package kata
+package main
 
 import (
-    "strings"
+    //"strings"
     "strconv"
 )
 
